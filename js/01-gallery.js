ba6cd072b88e9a8e61856419bf10bfa3b69c3887
qasklp <freeee386@gmail.com>
2022-10-30 11:31:56 +0200

    bug fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,9 +3,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector(".gallery");
 const render = galleryItems
-  .map(item => `<a class="gallery__item" href="${item.original}">
-  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>
+  .map(item => `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+        />
+    </a>
+    </div>
     `)
     .join("");
 gallery.insertAdjacentHTML("afterbegin", render);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,16 +4,9 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 const gallery = document.querySelector(".gallery");
 const render = galleryItems
-  .map(item => `<div class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
-        <img
-        class="gallery__image"
-        src="${item.preview}"
-        data-source="${item.original}"
-        alt="${item.description}"
-        />
-    </a>
-    </div>
+  .map(item => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
     `)
     .join("");
 console.log(render);
